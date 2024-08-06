0be8d9c494fe727ce4c1eaad7fdd66f6bad0513c
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-08-06 08:29:18 +0100

    A math floor function.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A math floor module."""
+
+def floor(n: float) -> int:
+    """A math floor function."""
+    if isinstance(n, int):
+        return n
+
+    if n > 0:
+        return int(n)
+    else:
+        t_n = int(n)
+        if n != t_n:
+            return t_n - 1
+        else:
+            return t_n
