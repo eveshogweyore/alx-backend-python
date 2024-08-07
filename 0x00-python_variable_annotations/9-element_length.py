5fd895fa73ed1716fdadf6b5b32602cc404c925a
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-08-07 10:43:12 +0100

    Add docstring to function.

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""A ducke type iterable object module."""
+"""A duck type iterable object module."""
 import typing
 
 
 def element_length(
     lst: typing.Iterable[typing.Sequence]
 ) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """A duck type iterable object function."""
     return [(i, len(i)) for i in lst]
