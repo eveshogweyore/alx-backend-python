5bf25f50b4572deba453e97830b1af7d1b81eeb5
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-08-07 09:36:05 +0100

    Meaningless change.

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """A sum of floats module."""
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """A sum of floats function."""
     result: float = 0.0
     for number in input_list:
